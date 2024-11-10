b2811c4ea439484f7e00df2961d148c37dc6e7ae
Brad Akemi <mirado.akemi@gmail.com>
2024-11-10 18:05:37 +0300

    setting

--- src/settings.py
+++ src/settings.py
@@ -151,6 +151,13 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://192.168.123.152",
+    "http://192.168.88.57",
+]
+CSRF_TRUSTED_ORIGINS = [
+   "http://localhost:3000",
+   "http://127.0.0.1:3000",
+"http://192.168.123.152",
+"http://192.168.88.57",
 ]
 
 # URL de base pour accéder aux fichiers médias
