5107f6cd9be31a5faa3dd9189ceb96a9b23092bd
Brad Akemi <mirado.akemi@gmail.com>
2024-11-22 14:33:38 +0300

    imagesModel

--- products/models.py
+++ products/models.py
@@ -54,7 +54,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="images/products/")
+    image = models.ImageField(upload_to="products/")
     alt_text = models.CharField(max_length=255, blank=True, null=True)
     is_primary = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
