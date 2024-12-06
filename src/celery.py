77f9c8ad08970c238d8365f856e8547bd1dad77d
Brad Akemi <mirado.akemi@gmail.com>
2024-12-06 14:18:34 +0300

    celery

--- src/celery.py
+++ src/celery.py
@@ -5,11 +5,10 @@ import os
 import ssl
 
 from celery import Celery
+from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
  
-from django.conf import settings
 
 app = Celery('src',
      broker_use_ssl = {
@@ -19,12 +18,14 @@ app = Celery('src',
         'ssl_cert_reqs': ssl.CERT_NONE
      })
  
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+app.conf.update(
+    worker_cancel_long_running_tasks_on_connection_loss=False,
+)
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
