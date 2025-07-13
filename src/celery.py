4a1d8303075639ae570f13868196a52c0bb51d6a
Brad Akemi <mirado.akemi@gmail.com>
2025-07-13 16:29:44 +0300

    src celery

--- src/celery.py
+++ src/celery.py
@@ -1,32 +1,19 @@
-# my_proj/my_proj/celery.py
+# src/celery.py
 from __future__ import absolute_import
 
 import os
-import ssl
 
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
- 
 
-app = Celery('src',
-     broker_use_ssl = {
-        'ssl_cert_reqs': ssl.CERT_NONE
-     },
-     redis_backend_use_ssl = {
-        'ssl_cert_reqs': ssl.CERT_NONE
-     })
- 
+app = Celery('src')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 app.conf.update(
     worker_cancel_long_running_tasks_on_connection_loss=False,
 )
-
-@app.task(bind=True, ignore_result=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
