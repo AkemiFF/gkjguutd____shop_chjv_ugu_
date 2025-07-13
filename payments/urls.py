5ba32f9dd62742699087f799898982cfe56104bf
Brad Akemi <mirado.akemi@gmail.com>
2025-07-13 16:30:20 +0300

    pay url stripe

--- payments/urls.py
+++ payments/urls.py
@@ -4,13 +4,13 @@ from .views import *
 from .webhooks import handle_payment_notification
 
 urlpatterns = [
-    path("get-token/", get_token, name="get_token"),
+    path('stripe/client-secret/', get_stripe_client_secret, name='stripe_client_secret'),
+    path('stripe/create-checkout/', create_checkout_session, name='stripe_checkout'),
     path("init-payment/", init_payment, name="init_payment"),
     path("init-cart-payment/", init_cart_payment, name="init_cart_payment"),
-    path("webhook/", handle_payment_notification, name="payment_notification"),    
+    path("webhook/", handle_payment_notification, name="stripe_webhook"),    
     path("check/<str:reference>/", CheckOrderPayment.as_view(), name="check_payment"),
-    path("test/", init_test, name="init_test"),
-    path("async-cart-pay/", init_cart_payment2, name="init_test"),
+    path("async-cart-pay/", init_cart_payment_stripe, name="init_init_cart_payment_stripe"),
     path("async-ref-pay/", init_ref_payment, name="init_ref_payment"),
     path("get-task-status/", get_task_status, name="get_task_status"),
     # path("api/response/pay", views.handle_payment_notification, name="handle_payment_notification"),
