162c15d66a5d679ae78862cb56955155f0f3bfbc
Brad Akemi <mirado.akemi@gmail.com>
2025-07-13 12:40:54 +0300

    activate sqlite

--- src/settings.py
+++ src/settings.py
@@ -10,7 +10,6 @@ CeleryAccess = config('CELERY_ACCESS')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY =config('DJANGO_SECRET_KEY')
 
 DEBUG = config('DEBUG')
@@ -22,6 +21,7 @@ ALLOWED_HOSTS = ["*"]
 ALLOWED_POINT = [
     "https://shoplg.online",
     "https://www.shoplg.online",
+    "http://localhost:8000",
 ]
 
 
@@ -102,22 +102,22 @@ WSGI_APPLICATION = "src.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default=5432, cast=int),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT', default=5432, cast=int),
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
